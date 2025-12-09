4c80efea09ad7c43f3c6affbad104744521ccb4b
Nora <nizcat5@gmail.com>
2025-12-09 14:27:37 -0800

    Maybe an app?

--- purrwatt/service-worker.js
+++ purrwatt/service-worker.js
@@ -12,3 +12,23 @@ self.addEventListener("push", event => {
     );
 });
 
+// Change this to your repository name
+var GHPATH = '/norazitnick.github.io/purrwatt';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'purr_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+//   `${GHPATH}/`,
+  `${GHPATH}/purrwatt-index.html`,
+  `${GHPATH}/purrwatt.css`,
+  `${GHPATH}/purrwatt.js`
+]
